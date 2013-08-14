77f3649fe77c903853670fb573f29f5646e370c9
Tom Dryer <tomdryer.com@gmail.com>
2013-08-13 21:15:59 -0700

    Remove black titlebar from maximized window
    Windows opened by window.open appear to get a black titlebar, while
    windows opened by local_path in the application's manifest get compact
    maximize/close controls with no titlebar. This change opens the crosh
    shell window using a javascript redirect from a window loaded from
    local_path, getting rid of the titlebar.
    
    Regressions:
    * window has no title
    * window has no default size
    * app opens in new tab by default

---
+++ loader.js
@@ -0,0 +1,4 @@
+// redirect to the secure shell app's crosh page
+window.location.replace(
+  "chrome-extension://pnhechapfaindjhompbnflcldabbghjo/html/crosh.html"
+);
--- main.js
+++
@@ -1,9 +0,0 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-  // crosh built-in: chrome-extension://nkoccljplnhpfnfiajclkommnmllphnl/
-  // Secure Shell: chrome-extension://pnhechapfaindjhompbnflcldabbghjo/
-  // Ssh dev: chrome-extension://okddffdblfhhnmhodogpojmfkjmhinfp/
-  window.open(
-      'chrome-extension://pnhechapfaindjhompbnflcldabbghjo/html/crosh.html', '',
-      'chrome=no,close=yes,resize=yes,scrollbars=yes,minimizable=yes,' +
-      'width=735,height=440');
-});
